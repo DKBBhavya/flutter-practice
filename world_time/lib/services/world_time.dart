278398582486db93b9de92d862091965013be25d
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-06-06 21:38:01 +0530

    Passed parameters

--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -10,16 +10,20 @@ class WorldTime {
   WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    Response response =
-        await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
+    try {
+      Response response =
+          await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
 
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    time = now.toString();
+      time = now.toString();
+    } catch (e) {
+      time = 'could not get time data';
+    }
   }
 }
